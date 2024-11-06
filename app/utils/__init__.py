0b3c5ae841b9efdfef755d48765709eae8f046f2
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-11-06 18:08:19 +0500

    add: utils functions

---
+++ app/utils/__init__.py
@@ -0,0 +1,17 @@
+from app.utils.utils import (
+    set_icon,
+    get_screen_size,
+    get_app_version,
+    guid,
+)
+from app.utils.manager import WindowsManager
+from app.utils.settings import Settings
+
+__all__ = [
+    "set_icon",
+    "get_screen_size",
+    "WindowsManager",
+    "get_app_version",
+    "guid",
+    "Settings",
+]
